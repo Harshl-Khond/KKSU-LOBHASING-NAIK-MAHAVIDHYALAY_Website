14c10bc10c3543a90213d5028dacbe4c275f5334
Harshal Khond <155840455+Harshl-Khond@users.noreply.github.com>
2026-01-22 12:51:11 +0530

    Update MainLayout.jsx

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,8 +1,9 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 // import "./MainLayout.css";
-import Footer from "../components/Footer";
+// import Footer from "../components/Footer";
 
 export default function MainLayout() {
   return (
